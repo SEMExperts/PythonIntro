2b01a07623fb9c096a3ec170e03ccda47435cbbc
Artem Zorin <marketing@semexperts.com.ua>
2020-09-10 09:17:08 +0300

    homework lesson 2

---
+++ homework/homework02_02.py
@@ -0,0 +1,14 @@
+print("\\n      new line")
+print("\\t      tab")
+print("\\b      backspace")
+print("\\'      '")
+print('\\"      "')
+print("\\\\      \\")
+print("\\a      alarm - ascii bell")
+
+
+
+
+
+
+
