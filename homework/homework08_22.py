cb99a464986addc0b311006c3b574f8458f3ccb6
Artem Zorin <marketing@semexperts.com.ua>
2020-10-05 22:54:32 +0300

    homework 22

---
+++ homework/homework08_22.py
@@ -0,0 +1,13 @@
+"""
+Написать функцию is_year_leap, принимающую 1 аргумент — номер года, и возвращающую True,
+если год високосный, и False иначе.
+"""
+
+def is_year_leap(y):
+    if y / 4 == y // 4:
+        return True
+    else:
+        return False
+
+a = int(input('Введите год: '))
+print('Год высокосный: ', is_year_leap(a))
